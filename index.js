49009a2cd752b83bb2e7be7ce99081d2abb1ad89
NavIsHeree <132490688+NavIsHeree@users.noreply.github.com>
2023-12-24 09:54:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+/* */ @format */
+
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "905003053oa347f9820077ebadat1974";
+
+  let prompt = `User instructions: Genrate English poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem page and NOT at the beginning";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating"> ⏳ Generating English poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
